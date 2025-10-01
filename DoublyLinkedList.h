94fd7d89f5114651fe7b58c747f82836db4e3851
yukiozono <yukionfire@gmail.com>
2025-10-01 13:43:13 +0900

    added public functions

---
+++ DoublyLinkedList.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <fstream>
+
+/**
+ * @class DoublyLinkedList
+ * @brief 指定したファイルを取り込める双方向リスト
+ *
+ * txtファイルを指定し、読み込むことで、
+ * 再度同順に出力できます
+ */
+template<class T>
+class DoublyLinkedList {
+public:
+	/**
+	 * @brief ファイル読み込み
+	 * @param ファイルパス
+	 */
+	void inputData(std::string txtName) {
+
+	}
+
+	/**
+	 * @brief 読み込んだ内容の出力
+	 * @return 文字列(string)
+	 */
+	std::string outputData() {
+
+	}
+private:
+
+};
