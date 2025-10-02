11b118d7509b8cee8e6b44cdfe4d61f4845e7832
yukiozono <yukionfire@gmail.com>
2025-10-02 11:45:36 +0900

    changed listSize from unsigned short to unsigned long long

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -9,150 +9,151 @@
  * txtファイルを指定し、読み込むことで、
  * 再度同順に出力できます
  */
-	class DoublyLinkedList {
-	public:
-		/**
-		 * @brief ファイル読み込み
-		 * @param ファイルパス
-		 */
-		void inputData(std::string filePath) {
-			//まずはリストの内容物クリア
-			clear();
+class DoublyLinkedList {
+public:
+	/**
+	 * @brief ファイル読み込み
+	 * @param ファイルパス
+	 */
+	void inputData(std::string filePath) {
+		//まずはリストの内容物クリア
+		clear();
 
-			//ファイルパスからファイルを開く
-			std::ifstream file(filePath.c_str());
+		//ファイルパスからファイルを開く
+		std::ifstream file(filePath.c_str());
 
-			//もし正しくファイルを開けなかった場合、return
-			if (!file.is_open()) {
-				return;
-			}
-
-			std::string line = {};
-
-			//一行ずつListに追加
-			while (std::getline(file, line)) {
-				pushback(line);
-			}
-
-			//ファイルを閉じる
-			file.close();
+		//もし正しくファイルを開けなかった場合、return
+		if (!file.is_open()) {
+			return;
 		}
 
-		/**
-		 * @brief 読み込んだ内容の出力
-		 * @return 文字列(string)
-		 */
-		std::string outputData() {
-			std::string outputLine;
-			Node* current = head;
+		std::string line = {};
 
-			//currentがnullptrになるまでループ
-			while (current != nullptr) {
-				//文字列を追加し、改行
-				outputLine += current->line;
-				outputLine += '\n';
-
-				//次のノードへ
-				current = current->nextNode;
-			}
-
-			//文字列を返す
-			return outputLine;
+		//一行ずつListに追加
+		while (std::getline(file, line)) {
+			pushback(line);
 		}
 
-		/**
-		 * @brief デフォルトコンストラクタ。
-		 */
-		DoublyLinkedList() = default;
+		//ファイルを閉じる
+		file.close();
+	}
 
-		/**
-		 * @brief デストラクタでリストの内容物全消去
-		 */
-		~DoublyLinkedList() {
-			clear();
+	/**
+	 * @brief 読み込んだ内容の出力
+	 * @return 文字列(string)
+	 */
+	std::string outputData() {
+		std::string outputLine;
+		Node* current = head;
+
+		//currentがnullptrになるまでループ
+		while (current != nullptr) {
+			//文字列を追加し、改行
+			outputLine += current->line;
+			outputLine += '\n';
+
+			//次のノードへ
+			current = current->nextNode;
 		}
 
-		/**
-		 * @brief コピーコンストラクタを削除。
-		 */
-		DoublyLinkedList(const DoublyLinkedList&) = delete;
+		//文字列を返す
+		return outputLine;
+	}
 
-		/**
-		 * @brief コピー代入演算子を削除。
-		 */
-		DoublyLinkedList& operator=(const DoublyLinkedList&) = delete;
+	/**
+	 * @brief デフォルトコンストラクタ。
+	 */
+	DoublyLinkedList() = default;
 
-	private:
-		struct Node {
-			std::string line = {};  //ノード内の内容
-			Node* prevNode = nullptr;    //一つ前のノードのポインタ
-			Node* nextNode = nullptr;    //一つ後のノードのポインタ
-		};
+	/**
+	 * @brief デストラクタでリストの内容物全消去
+	 */
+	~DoublyLinkedList() {
+		clear();
+	}
 
-		Node* head = nullptr;  //先頭アドレス
-		Node* tail = nullptr;  //末尾アドレス
-		unsigned short listSize = 0;  //現在のリストのサイズ
+	/**
+	 * @brief コピーコンストラクタを削除。
+	 */
+	DoublyLinkedList(const DoublyLinkedList&) = delete;
 
-		/**
-		 * @brief ノード追加
-		 * @param previous 挿入先のノード
-		 * @param current  挿入するノード
-		 */
-		void addNode(Node* previous, Node* current) {
+	/**
+	 * @brief コピー代入演算子を削除。
+	 */
+	DoublyLinkedList& operator=(const DoublyLinkedList&) = delete;
 
-			//もしリストが空であった場合
-			if (listSize == 0) {
-				//先頭と末尾アドレスにcurrentを代入
-				head = current;
-				tail = current;
-				current->prevNode = current->nextNode = nullptr;
-			}
-			//もしpreviousの次のノードが末尾のノードではなかった場合(途中からの挿入の場合)
-			else if (previous != tail) {
-				//previousのノードとcurrentの前後のアドレスを再編成
-				current->nextNode = previous->nextNode;
-				previous->nextNode = current;
-				current->prevNode = previous;
+private:
+	struct Node {
+		std::string line = {};  //ノード内の内容
+		Node* prevNode = nullptr;    //一つ前のノードのポインタ
+		Node* nextNode = nullptr;    //一つ後のノードのポインタ
+	};
 
-				//次のノードの前要素アドレスにcurrentを代入
-				current->nextNode->prevNode = current;
-			}
-			//もしpreviousが末尾のノードだった場合
-			else if (previous == tail) {
-				//currentを追加し、末尾アドレスにcurrentを代入
-				previous->nextNode = current;
-				current->prevNode = previous;
-				current->nextNode = nullptr;
-				tail = current;
-			}
+	Node* head = nullptr;  //先頭アドレス
+	Node* tail = nullptr;  //末尾アドレス
+	unsigned long long listSize = 0;  //現在のリストのサイズ
 
-			//リストサイズを管理する変数を+1
-			listSize++;
+	/**
+	 * @brief ノード追加
+	 * @param previous 挿入先のノード
+	 * @param current  挿入するノード
+	 */
+	void addNode(Node* previous, Node* current) {
+
+		//もしリストが空であった場合
+		if (listSize == 0) {
+			//先頭と末尾アドレスにcurrentを代入
+			head = current;
+			tail = current;
+			current->prevNode = current->nextNode = nullptr;
+		}
+		//もしpreviousの次のノードが末尾のノードではなかった場合(途中からの挿入の場合)
+		else if (previous != tail) {
+			//previousのノードとcurrentの前後のアドレスを再編成
+			current->nextNode = previous->nextNode;
+			previous->nextNode = current;
+			current->prevNode = previous;
+
+			//次のノードの前要素アドレスにcurrentを代入
+			current->nextNode->prevNode = current;
+		}
+		//もしpreviousが末尾のノードだった場合
+		else if (previous == tail) {
+			//currentを追加し、末尾アドレスにcurrentを代入
+			previous->nextNode = current;
+			current->prevNode = previous;
+			current->nextNode = nullptr;
+			tail = current;
 		}
 
-		/**
-		 * @brief リストに要素追加
-		 * @param newLine 追加する文字列
-		 */
-		void pushback(const std::string& newLine) {
-			Node* node = new Node;
-			node->line = newLine;
-			addNode(tail, node);
-		}
+		//リストサイズを管理する変数を+1
+		listSize++;
+	}
 
-		/**
-		 * @brief リスト内の要素先頭から全消去
-		 */
-		void clear() {
-			Node* current = head;
-			//currentがnullptrになるまでループし、ノードを削除
-			while (current != nullptr) {
-				Node* next = current->nextNode;
-				delete current;
-				current = next;
-			}
-			head = nullptr;
-			tail = nullptr;
-			listSize = 0;
+	/**
+	 * @brief リストに要素追加
+	 * @param newLine 追加する文字列
+	 */
+	void pushback(const std::string& newLine) {
+		Node* node = new Node;
+		node->line = newLine;
+		addNode(tail, node);
+	}
+
+	/**
+	 * @brief リスト内の要素先頭から全消去
+	 */
+	void clear() {
+		Node* current = head;
+		//currentがnullptrになるまでループし、ノードを削除
+		while (current != nullptr) {
+			Node* next = current->nextNode;
+			delete current;
+			current = next;
 		}
+		head = nullptr;
+		tail = nullptr;
+		listSize = 0;
+	}
+
 };
